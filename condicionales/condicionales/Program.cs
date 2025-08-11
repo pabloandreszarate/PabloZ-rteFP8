43b93df4b3954a36fa814c812f9a20aafc222898
pabloandreszarate <158299580+pabloandreszarate@users.noreply.github.com>
2025-08-11 16:00:54 -0500

    Add files via upload
    condicionales simples queda como ejercicio para la próxima clase

---
+++ condicionales/condicionales/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace condicionales
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Condicionales simples
+            //1 Crear un algortimo que permita ingresar el nombre y el sueldo de una persona si el sueldo supera los 300 pesos mostrar el mensaje en pantalla con el nombre de la persona, indicando que debe abonar impuestos.*/
+
+            string nombre = "";
+            int sueldo = 0;
+
+            Console.WriteLine("Ingrese su nombre");
+            nombre = Console.ReadLine(); //todo lo que se lee desde la consola llega como un string
+            Console.WriteLine("Ingrese su sueldo");
+            sueldo = Convert.ToInt32(Console.ReadLine());
+
+            if (sueldo > 3000)
+            {
+                Console.WriteLine($"La persona {nombre}, debe abonar impuestos");
+            }
+            if (sueldo < 3000)
+            {
+                Console.WriteLine($"Ah bueno, {nombre}, gracias");
+            }
+
+            //Crea un algoritmo que lea la edad de un usuario, si el usuario es mayor de 18 se debe mostrar el siguiente mensaje: "Bienvenido a mi sitio web"
+
+            int edad = 0;
+
+            Console.WriteLine("¿Cuántos años tiene?");
+            edad = Convert.ToInt32(Console.ReadLine());
+
+            if (edad > 18)
+            {
+                Console.WriteLine($"Bienvenido a mi sitio web");
+            }
+
+        }
+    }
+}
