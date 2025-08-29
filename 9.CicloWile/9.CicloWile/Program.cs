d94a33ae53fa291c5defad6c5668036269514e72
pabloandreszarate <158299580+pabloandreszarate@users.noreply.github.com>
2025-08-29 09:13:50 -0500

    Add files via upload
    CicloWille

---
+++ 9.CicloWile/9.CicloWile/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _9.CicloWile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 0;
+            int acumulador = 0;
+
+            while (contador <= 5)
+            {
+                acumulador += contador;
+                Console.WriteLine($"contador:: {contador} - acumulador: {acumulador}");
+                contador++;
+
+            }
+
+            Console.WriteLine($"la suma de los primeros 5 numeros es: + {acumulador}");
+        }
+    }
+}
