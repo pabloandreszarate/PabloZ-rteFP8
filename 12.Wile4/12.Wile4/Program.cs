826bc4c6cf0075672694519e2e15bed1a9fd7d8b
pabloandreszarate <158299580+pabloandreszarate@users.noreply.github.com>
2025-09-03 08:24:41 -0500

    Add files via upload
    Ejercicio de while numero 4

---
+++ 12.Wile4/12.Wile4/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _12.Wile4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 1;
+            int suma = 0;
+
+            while (contador <=99)
+            {
+                suma += contador;
+                Console.WriteLine($"{contador} - {suma} ");
+                contador += 2;
+                Console.WriteLine($"la suma de todos los numeros impares hasta 99 es {suma}");
+            }
+        }
+    }
+}
