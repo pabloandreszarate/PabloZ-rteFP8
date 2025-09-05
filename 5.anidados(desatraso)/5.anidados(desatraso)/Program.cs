49e20d4a5fceac86cea81f202dacdfbfcc8e5b13
pabloandreszarate <158299580+pabloandreszarate@users.noreply.github.com>
2025-09-05 15:54:07 -0500

    Add files via upload
    Desatraso de la actividad numero 5

---
+++ 5.anidados(desatraso)/5.anidados(desatraso)/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _5.anidados_desatraso_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            int num2 = 0;
+            int num3 = 0;
+
+            Console.WriteLine("Ingrese el numero 1");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el numero 2");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el numero 3");
+            num3 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                if (num1 > num3)
+                {
+                    Console.WriteLine($"el numero {num1}, es el mayor");
+                }
+                else
+                {
+                    Console.WriteLine($"el numero {num2}, es el mayor");
+                }
+            }
+            else
+            {
+                if (num2 > num3)
+                {
+
+                    Console.WriteLine($"el numero {num2}, es el mayor");
+
+                }
+                else
+                {
+                    Console.WriteLine($"el numero {num3}, es el mayor");
+                }
+            }
+    }
+}
